fc2a2f6cfbec87dad13fdce0c5b9a37dd22a565e
Gabriel F. Vilar <gabriel@cogumm.net>
2020-11-09 19:50:40 -0300

    Client Side Feching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
+interface IProduct {
+  id: string;
+  title: string;
+}
 export default function Home() {
+  const [recomendedProducts, setRecomendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/recommended').then(res => {
+      res.json().then(data => {
+        setRecomendedProducts(data);
+      })
+    })
+  }, []);
+
   return (
     <div>
       <Title>Estou vivo!</Title>
+
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recomendedProducts.map(recomendedProduct => {
+            return (
+              <li key={recomendedProduct.id}>
+                {recomendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </div>
   )
 }
